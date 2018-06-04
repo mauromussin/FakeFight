e27ed1ab410fbb8b28407659878648e1fbfe0a02
mauromussin <m.mussin@arpalombardia.it>
2018-06-05 00:08:17 +0200

    initial commit

--- test.py
+++ test.py
@@ -8,10 +8,10 @@ Script di test del servizio
 """
 
 from TwitterAPI import TwitterAPI
-consumer_key='bpprsnAw5J0VfMVMsMoltEjvn'
-consumer_secret='LyXoJx6IeO0s9tfA7yNaDOEIzJN2qTdYYMVTpAQxsCBPkXYARw'
-access_token_key='247889299-tzSe0Wh84jOiRCN7yS0FixrhLMsTCutQ7oNI2NhB'
-access_token_secret='71JYKcBK1QfFKGysg8Djuld39MpN1MDfXxxQetJ2IjMzz'
+consumer_key='put something here'
+consumer_secret='put something here'
+access_token_key='put something here'
+access_token_secret='put something here'
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 #r = api.request('statuses/update', {'status':'FakeFight from Demo_FF_test!'})
 r = api.request('search/tweets', {'q':'#Governo'})
